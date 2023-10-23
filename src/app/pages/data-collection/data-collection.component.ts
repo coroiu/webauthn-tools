215ee149961d9829f270a945eaed78d44171a278
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-23 10:43:00 +0200

    feat: add script to web page

--- src/app/pages/data-collection/data-collection.component.ts
+++ src/app/pages/data-collection/data-collection.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   templateUrl: './data-collection.component.html',
+  imports: [CommonModule, HttpClientModule],
   standalone: true,
 })
-export class DataCollectionComponent {}
+export class DataCollectionComponent {
+  script$: Observable<string>;
+
+  constructor(httpClient: HttpClient) {
+    this.script$ = httpClient
+      .get('assets/connectors/console-based.js', { responseType: 'text' })
+      .pipe(map((script) => `eval(atob("${btoa(script)}"))`));
+  }
+}
