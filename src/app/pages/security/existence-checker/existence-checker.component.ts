3cdd6b8a2fe8b6cef4e8d867660cf4458129976c
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-11-28 10:10:38 +0100

    Add provider existence display

--- src/app/pages/security/existence-checker/existence-checker.component.ts
+++ src/app/pages/security/existence-checker/existence-checker.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
 import { getCoroiu } from '../../../types/window-coroiu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   templateUrl: './existence-checker.component.html',
+  styleUrls: ['./existence-checker.component.scss'],
+  imports: [CommonModule, MatCardModule],
   standalone: true,
 })
 export class ExistenceCheckerComponent implements OnInit {
