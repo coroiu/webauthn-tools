4cfa1dab1d6d55f88e8fdb7a02e1b353390b79e3
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-11-28 09:54:50 +0100

    Add passkey provider detection

--- src/app/pages/security/existence-checker/existence-checker.component.ts
+++ src/app/pages/security/existence-checker/existence-checker.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getCoroiu } from '../../../types/window-coroiu';
 
 @Component({
   templateUrl: './existence-checker.component.html',
   standalone: true,
 })
-export class ExistenceCheckerComponent {}
+export class ExistenceCheckerComponent implements OnInit {
+  protected passkeyProviderDetected = false;
+
+  ngOnInit(): void {
+    this.passkeyProviderDetected = this.checkPasskeyProvider();
+  }
+
+  private checkPasskeyProvider(): boolean {
+    return window.navigator.credentials.get !== getCoroiu().webauthn.native.get;
+  }
+}
---
+++ src/assets/browser-modifications/webauthn-injection.js
@@ -0,0 +1,26 @@
+window.coroiu.webauthn = {
+  native: {
+    get: window.navigator.credentials.get,
+    create: window.navigator.credentials.create,
+    credentialsContainer: window.navigator.credentials,
+  },
+  // interceptors: {},
+};
+
+// window.navigator.credentials.get = (options, abortController) => {
+//   const interceptor = window.coroiu.webauthn.interceptors.get;
+//   if (interceptor) {
+//     return interceptor(options, abortController);
+//   } else {
+//     return window.coroiu.webauthn.native.get(options, abortController);
+//   }
+// };
+
+// window.navigator.credentials.create = (options, abortController) => {
+//   const interceptor = window.coroiu.webauthn.interceptors.create;
+//   if (interceptor) {
+//     return interceptor(options, abortController);
+//   } else {
+//     return window.coroiu.webauthn.native.create(options, abortController);
+//   }
+// };
