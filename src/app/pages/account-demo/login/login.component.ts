6e523b871772165d22b1d9c07bdecfe8d5bfac4e
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-22 14:34:20 +0200

    feat: detect capabilities

--- src/app/pages/account-demo/login/login.component.ts
+++ src/app/pages/account-demo/login/login.component.ts
@@ -3,6 +3,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -15,18 +16,22 @@ import { Router } from '@angular/router';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatCheckboxModule,
     FormsModule,
   ],
 })
 export class LoginComponent implements OnInit {
   protected username: string = '';
   protected password: string = '';
+  protected conditionalCreate: boolean = false;
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   login() {
-    void this.router.navigate(['/account-demo/profile', this.username]);
+    void this.router.navigate(['/account-demo/profile', this.username], {
+      queryParams: { conditionalCreate: this.conditionalCreate },
+    });
   }
 }
--- src/app/pages/account-demo/profile/profile.component.ts
+++ src/app/pages/account-demo/profile/profile.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 
+type Capabilities = {
+  conditionalCreate?: boolean;
+};
+
 @Component({
   templateUrl: './profile.component.html',
   standalone: true,
+  imports: [CommonModule],
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
 
   protected username: string = '';
+  protected capabilities: Capabilities = {};
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,6 +24,33 @@ export class ProfileComponent implements OnInit, OnDestroy {
     this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe((params) => {
       this.username = params.get('username') || '';
     });
+
+    this.route.queryParamMap
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(async (params) => {
+        if (params.get('conditionalCreate') === 'true') {
+          this.autoCreatePasskey();
+        }
+      });
+  }
+
+  private async autoCreatePasskey() {
+    if ((PublicKeyCredential as any).getClientCapabilities === undefined) {
+      console.log('Conditional creation unavailable');
+      this.capabilities.conditionalCreate = false;
+      return;
+    }
+
+    const capabilities = await (
+      PublicKeyCredential as any
+    ).getClientCapabilities();
+    if (!capabilities.conditionalCreate) {
+      console.log('Conditional creation unavailable');
+      this.capabilities.conditionalCreate = false;
+      return;
+    }
+
+    this.capabilities.conditionalCreate = true;
   }
 
   ngOnDestroy(): void {
