d5bbc89b23d04393149826dc7083414870b28b06
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-22 13:51:47 +0200

    feat: add login form

--- src/app/pages/account-demo/login/login.component.ts
+++ src/app/pages/account-demo/login/login.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
   standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
 })
 export class LoginComponent implements OnInit {
   constructor() {}
